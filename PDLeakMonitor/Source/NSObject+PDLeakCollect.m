29090c295b775d61fd91cc2d8573ea66210ec4c5
liang <18811130435@163.com>
2019-12-25 19:45:41 +0800

    add property info

--- PDLeakMonitor/Source/NSObject+PDLeakCollect.m
+++ PDLeakMonitor/Source/NSObject+PDLeakCollect.m
@@ -8,6 +8,7 @@
 
 #import "NSObject+PDLeakCollect.h"
 #import <objc/runtime.h>
+#import <objc/message.h>
 #import "PDClassInfo.h"
 #import "PDLeakConfiguration.h"
 #import "NSObject+PDLeakProxy.h"
@@ -22,13 +23,18 @@
             
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
         NSMutableArray<PDClassIvarInfo *> *ivarInfos = [NSMutableArray array];
+        NSMutableArray<PDClassPropertyInfo *> *propertyInfos = [NSMutableArray array];
 
         // Find all properties to rootClass.
         Class cls = [self class];
         while (cls && ![configuration classShouldBeIgnore:cls]) {
             PDClassInfo *classInfo = [PDClassInfo classInfoWithClass:cls];
-            NSArray *values = classInfo.ivarInfos.allValues;
-            [ivarInfos addObjectsFromArray:values];
+            
+            NSArray *ivars = classInfo.ivarInfos.allValues;
+            [ivarInfos addObjectsFromArray:ivars];
+            
+            NSArray *properties = classInfo.propertyInfos.allValues;
+            [propertyInfos addObjectsFromArray:properties];
             
             cls = [cls superclass];
         }
@@ -44,6 +50,23 @@
                 if (!ivarIsRetained) { continue; }
 
                 id value = object_getIvar(self, ivarInfo.ivar);
+                if (!value) { continue; }
+                
+                if ([value markingAlive]) {
+                    [value pd_leakProxy].owner = self;
+                    [value pd_collectAllRetainedIvarsWithLevel:level + 1];
+                }
+            }
+            
+            for (PDClassPropertyInfo *propertyInfo in propertyInfos) {
+                BOOL propertyIsObject = ((propertyInfo.type & PDEncodingTypeMask) == PDEncodingTypeObject);
+                if (!propertyIsObject) { continue; }
+                
+                BOOL propertyIsRetained = ((propertyInfo.type & PDEncodingTypePropertyMask) == PDEncodingTypePropertyRetain ||
+                                           (propertyInfo.type & PDEncodingTypePropertyMask) == PDEncodingTypePropertyCopy);
+                if (!propertyIsRetained) { continue; }
+
+                id value = ((id (*)(id, SEL))(void *) objc_msgSend)((id)self, propertyInfo.getter);
                 if (!value) { continue; }
                 
                 if ([value markingAlive]) {
